6e3300af52d89a939f02546de06895774a38903d
beingshuchi <shuchitamail@gmail.com>
2018-08-06 18:04:26 +0530

    Update Customer.java

--- 152919_Phase3/src/main/java/com/cg/bean/Customer.java
+++ 152919_Phase3/src/main/java/com/cg/bean/Customer.java
@@ -1,50 +1,60 @@
 package com.cg.bean;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Customer {
-	private String name;
+	String name;
 	@Id
-	private String mobileNo;
-	@OneToOne
-	private Wallet wallet;
+	String mobileNo;
+	@OneToOne(cascade=CascadeType.ALL)
+	Wallet wallet;
+	
+	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getMobileNo() {
 		return mobileNo;
 	}
+
 	public void setMobileNo(String mobileNo) {
 		this.mobileNo = mobileNo;
 	}
+
 	public Wallet getWallet() {
 		return wallet;
 	}
+
 	public void setWallet(Wallet wallet) {
 		this.wallet = wallet;
 	}
 	
-	@Override
-	public String toString() {
-		return "Customer [name=" + name + ", mobileNo=" + mobileNo + ", wallet=" + wallet
-				+ "]";
-	}
+
 	public Customer(String name, String mobileNo, Wallet wallet) {
 		super();
 		this.name = name;
 		this.mobileNo = mobileNo;
 		this.wallet = wallet;
 	}
+
 	public Customer() {
-		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", mobileNo=" + mobileNo + ", wallet=" + wallet + "]";
+	}
 	
 }
